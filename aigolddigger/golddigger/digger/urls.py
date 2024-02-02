f66b6bfe1ef8f13e2f01841694da60332b410615
efesdark <2020032031@student.beykent.edu.tr>
2024-02-02 23:34:09 +0300

    added prices chart

--- aigolddigger/golddigger/digger/urls.py
+++ aigolddigger/golddigger/digger/urls.py
@@ -1,11 +1,12 @@
 # myapp/urls.py
 from django.urls import path
-from .views import home, get_bitcoin_price, chart
+from .views import home, get_bitcoin_price, chart, charts
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('get_bitcoin_price/', get_bitcoin_price, name='get_bitcoin_price'),
     path('chart/', chart, name='chart'),
+    path('charts/', charts, name='charts'),
 ]
 
--- aigolddigger/golddigger/digger/views.py
+++ aigolddigger/golddigger/digger/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import ccxt
-
+import requests
 def home(request):
     return render(request, 'home.html')
 
 def chart(request):
     return render(request, 'chart.html') 
+from django.shortcuts import render
+import requests
+
+def charts(request):
+  # Binance API'dan canlı bitcoin fiyatlarını çekmek için bir istek yapın
+    binance_api_url = 'https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT'
+    response = requests.get(binance_api_url)
+    data = response.json()
+
+    # Veriyi işleyerek grafikte kullanılabilir formata dönüştürün
+    current_price = float(data['lastPrice'])
+    change_percentage = float(data['priceChangePercent'])
+    high_price = float(data['highPrice'])
+    low_price = float(data['lowPrice'])
+    volume = float(data['volume'])
+    volume_usdt = float(data['quoteVolume'])
+
+    # Template'e gönderilecek context oluşturun
+    context = {
+        'current_price': current_price,
+        'change_percentage': change_percentage,
+        'high_price': high_price,
+        'low_price': low_price,
+        'volume': volume,
+        'volume_usdt': volume_usdt,
+    }
+
+    # HTML template'i ile birlikte render edin
+    return render(request, 'chart.html', context)
+
+
 
 def get_bitcoin_price(request):
     # Binance API'ye istek atmak için ccxt kütüphanesini kullanalım
@@ -25,3 +56,4 @@ def get_bitcoin_price(request):
 
     except ccxt.ExchangeError as e:
         return JsonResponse({'error': f'Exchange hatası: {e}'})
+        
